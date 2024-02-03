f7d9eb5a60c7758769c02338f60e19fb8ea54b6f
Rajnish-J <rajalehe7102@gmail.com>
2024-02-03 22:18:35 +0530

    saturday changes

---
+++ practice_codechef/500 to 1000/mario_1.java
@@ -0,0 +1,17 @@
+// Mario and Bullet
+
+// Mario's bullet moves at X pixels per frame. He wishes to shoot a goomba standing Y pixels away from him. The goomba does not move.
+// Find the minimum time (in seconds) after which Mario should shoot the bullet, 
+// such that it hits the goomba after at least Z seconds from now.
+
+import java.util.*;
+
+class mario{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while (t-->0){
+            
+        }
+    }
+}
