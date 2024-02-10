06e25b72e6a9075486851a433c23978973a45c19
Rajnish-J <rajalehe7102@gmail.com>
2024-02-10 19:13:57 +0530

    prior

---
+++ practice.java
@@ -0,0 +1,12 @@
+class appform{
+    public static void main(String[] args) {
+        appform rajnish = new appform();
+        rajnish.display("Rajnish", 334);
+        
+    }
+    public void display(String name, int rollno){
+        System.out.println(name);
+        System.out.println(rollno);
+    }
+
+}
