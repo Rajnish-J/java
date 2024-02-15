178549c5575ab806cf5c1185ee4addd2c0aaf7ca
Rajnish-J <rajalehe7102@gmail.com>
2024-02-15 16:35:56 +0530

    utube

--- java_mam_practice/array/largest_no.java
+++ java_mam_practice/array/largest_no.java
@@ -1,27 +1,15 @@
-import java.util.Scanner;
-
-class largest_no {
+class largest{
     public static void main(String[] args) {
-
-        Scanner sc = new Scanner(System.in);
-
-        int size_array = sc.nextInt();
-        
-        int [] array = new int[size_array];
+        int[] array = {11,25,45,78,4};
         int largest = 0;
         int size = array.length;
 
-        for (int i = 0; i<size ; i++){
-            array[i] = sc.nextInt();
-        }
-
-        for (int i = 0; i < size;i++){
+        //chcking loop
+        for(int i = 0; i<size; i++){
             if(array[i] > largest){
                 largest = array[i];
             }
         }
-        System.out.println(largest);
-        System.out.println("largest");
+        System.out.println("The largest element in the array: " + largest);
     }
-    
 }
