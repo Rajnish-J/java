b2955b033e3d0001374cc495246373a7ca0578b5
Rajnish-J <rajalehe7102@gmail.com>
2024-02-21 19:24:34 +0530

    yuitube

--- java_mam_practice/basic_programs/set 2/two_matrix.java
+++ java_mam_practice/basic_programs/set 2/two_matrix.java
@@ -3,35 +3,39 @@ import java.util.*;
 class matrix{
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        // size of the array:
+        System.out.print("Enter the size of the array: ");
         int size = sc.nextInt();
 
-        // initializing two matrix
+        // declaring two integer matrix:
         int[][] A = new int[size][size];
         int[][] B = new int[size][size];
-        //result matrix
+        // declaring for the result:
         int[][] C = new int[size][size];
-        
-        // input for first matrix
-        System.out.println("Enter input for first matrix: ");
-        for(int i = 0; i<size; i++){
+
+        // userinput or initializing
+        System.out.print("Enter the first matrix: ");
+        for(int i = 0;i<size; i++){
             for(int j = 0; j<size; j++){
                 A[i][j] = sc.nextInt();
             }
         }
-
-        // input for second matrix
-        System.out.println("Enter input for second matrix: ");
-        for(int i = 0; i<size; i++){
+        System.out.print("Enter the second matrix: ");
+        for(int i = 0;i<size; i++){
             for(int j = 0; j<size; j++){
                 B[i][j] = sc.nextInt();
             }
         }
 
-        // addition of two matrices
-        System.out.println("output: ");
-        for(int i = 0; i<size; i++){
-            for(int j = 0; j<size ; j++){
+        // adding two matrix:
+        for(int i = 0;i<size; i++){
+            for(int j = 0; j<size; j++){
                 C[i][j] = A[i][j] + B[i][j];
+            }
+        }
+        //showing output:
+        for(int i = 0; i<size; i++){
+            for(int j = 0; j<size; j++){
                 System.out.print(C[i][j] + " ");
             }
             System.out.println();
--- java_mam_practice/basic_programs/set 2/vowels.java
+++ java_mam_practice/basic_programs/set 2/vowels.java
@@ -6,16 +6,13 @@ class vowels{
 
         for(int i = 0; i<name.length();i++){
             char c = name.charAt(i);
-            if(c == 'a' || c == 'e' || c == 'i' || c == 'o'
-            || c == 'u' || c == 'A' || c == 'E' || c == 'I'
-            || c == 'O' || c == 'U'){
+            if(c == 'a' || c == 'e' || c == 'i' || c == 'o'|| c == 'u' || c == 'A' || c == 'E' || c == 'I'|| c == 'O' || c == 'U'){
                 vowels++;
             }
             else{
                 consonants++;
             }
         }
-        System.out.println("The given " + name + " contain "
-        + vowels + " vowels " + consonants + " consonants.");
+        System.out.println("The given " + name + " contain "+ vowels + " vowels " + consonants + " consonants.");
     }
 }
