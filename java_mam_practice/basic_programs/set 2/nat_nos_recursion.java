8e066f2ebc59a9af5f7f66705187f462c700efa7
Rajnish-J <rajalehe7102@gmail.com>
2024-02-23 17:21:29 +0530

    changes

--- java_mam_practice/basic_programs/set 2/nat_nos_recursion.java
+++ java_mam_practice/basic_programs/set 2/nat_nos_recursion.java
@@ -1,20 +1,18 @@
-// Sum of Natural Numbers using recursion
-
+// finding sum of natural numbers using recursion
 import java.util.*;
-
-class numbers{
+class nat_no{
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int num = sc.nextInt();
-        int sum = calculatenatural(num);
+        int sum = calculate_nat(num);
         System.out.println(sum);
     }
-    public static int calculatenatural(int n){
+    public static int calculate_nat(int n){
         if(n == 1){
             return 1;
         }
         else{
-            return n + calculatenatural(n-1);
+            return n + calculate_nat(n-1);
         }
     }
 }
