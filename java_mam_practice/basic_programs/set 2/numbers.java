ed5792d774490bd1d60f021d1d79b7517e57d25b
Rajnish-J <rajalehe7102@gmail.com>
2024-02-26 16:27:49 +0530

    recorded

--- java_mam_practice/basic_programs/set 2/numbers.java
+++ java_mam_practice/basic_programs/set 2/numbers.java
@@ -1,8 +1,12 @@
-class check{
-    public static void main(String[] args) {
-        int num = 98765;
+import java.util.*;
 
-        while(num > 0){
+class math{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the number: ");
+        Long num = sc.nextLong();
+
+        while(num>0){
             System.out.println(num);
             num /= 10;
         }
