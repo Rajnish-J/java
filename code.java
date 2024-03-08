6f54715094eb4c470e2226af28c25977fb0945a9
Rajnish-J <rajalehe7102@gmail.com>
2024-03-08 18:15:30 +0530

    codechef

--- code.java
+++
@@ -1,9 +0,0 @@
-class virat{
-    public static void main(String[] args) {
-        int a=20, b=40;
-        boolean res = ((a++ > b) && (b++ <a));
-        System.out.println(a);
-        System.out.println(b);
-        System.out.println(res);
-    }
-}
---
+++ practice_codechef/500 to 1000/polynomial.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+class Codechef
+{
+	public static void main (String[] args)
+	{
+		Scanner sc =new Scanner(System.in);
+		int t=sc.nextInt();
+		while(t-->0){
+		 int size=sc.nextInt();
+		 int a[]= new int[size];
+		 int max=0;
+		 for (int i=0;i<size ;i++ ){
+		     a[i]=sc.nextInt();
+		     if(a[i]!=0){
+		         
+		         max=i;
+		     }
+		 } 
+		 System.out.println(max);
+		}
+	}
+}
