f7a263025c6ea95e50ac0f2bf549fa718ed21800
Rajnish-J <rajnishofficial02@gmail.com>
2024-06-22 08:33:14 +0530

    array problems

---
+++ qspiders/practice/array/First_N_Primes.java
@@ -0,0 +1,40 @@
+package array;
+
+import java.util.*;
+
+class First_N_Primes {
+    static Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) {
+        
+        System.out.print("Enter the number of prime numbers you need: ");
+        int prime = sc.nextInt();
+        int[] primes = new int[prime];
+        int index = 0;
+        int num = 2;
+        
+        while (index < prime) {
+            if (isPrime(num)) {
+                primes[index] = num;
+                index++;
+            }
+            num++;
+        }
+        
+        System.out.println("The first 10 prime numbers are:");
+        for (int val : primes) {
+            System.out.print(val + " ");
+        }
+    }
+
+    public static boolean isPrime(int n) {
+        if (n <= 1) {
+            return false;
+        }
+        for (int i = 2; i <= Math.sqrt(n); i++) {
+            if (n % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
---
+++ qspiders/practice/array/even_odd.java
@@ -0,0 +1,38 @@
+package array;
+
+import java.util.*;
+
+class even_odd{
+    static Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) {
+        System.out.print("Enter the size of the array: ");
+        int size = sc.nextInt();
+        int[] arr = new int[size];
+        int even_count = 0, odd_count = 0;
+
+        for(int i = 0; i<arr.length; i++){
+            if(arr[i] % 2 ==0) even_count++;
+            else odd_count++;
+        }
+
+        int[] even_arr = new int[even_count];
+        int[] odd_arr = new int[odd_count];
+
+        odd_count = 0; even_count = 0;
+
+        for(int i = 0; i<arr.length; i++){
+            if(arr[i] % 2 == 0){
+                even_arr[even_count] = arr[i];
+                even_count++;
+            }
+            else{
+                odd_arr[odd_count] = arr[i];
+                odd_count++;
+            }
+        }
+        
+        System.out.println("Given array: " + Arrays.toString(arr));
+        System.out.println("Odd array: " + Arrays.toString(odd_arr));
+        System.out.println("Even array: " + Arrays.toString(even_arr));        
+    }
+}
