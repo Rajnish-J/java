f93102ef983f425c96b99faf26ae45a4d2050f3f
Rajnish-J <rajnishofficial02@gmail.com>
2024-11-30 12:37:45 +0530

    reverse check

---
+++ DSA prep/DSA_fundamentals/11_Graph/demo.java
@@ -0,0 +1,13 @@
+public class demo {
+    public static void main(String[] args) {
+
+        // ! comparision
+        if (18 > 7) {
+            System.out.println("Virat");
+        }
+    }
+}
+
+/*  output:
+    Virat
+ */
---
+++ DSA prep/DSA_fundamentals/11_Graph/rottenMangoes.java
@@ -0,0 +1,15 @@
+import java.util.LinkedList;
+
+public class rottenMangoes {
+    public static void main(String[] args) {
+        int[][] arr = { { 0, 1, 1, 0 }, { 2, 1, 0, 0 }, { 1, 1, 2, 0 }, { 1, 1, 1, 1 } };
+        System.out.println(rottingmangoes(arr));
+    }
+
+    public static int rottingmangoes(int[][] arr) {
+        int ret = 0;
+        LinkedList
+        return ret;
+    }
+
+}
